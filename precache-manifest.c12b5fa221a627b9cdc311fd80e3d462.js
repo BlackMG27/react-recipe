2287dcbaa7e51c0594f15fb0f932ea62931aa4cb
BlackMG27 <oike279@gmail.com>
2019-05-07 08:30:22 -0500

    Updates

---
+++ precache-manifest.c12b5fa221a627b9cdc311fd80e3d462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da743ac103e8eb0307104fdbd1f5aa90",
+    "url": "/react-recipe/index.html"
+  },
+  {
+    "revision": "e0cac92cda281380167d",
+    "url": "/react-recipe/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "79d6c423452cdd7c366f",
+    "url": "/react-recipe/static/js/2.d8d51273.chunk.js"
+  },
+  {
+    "revision": "e0cac92cda281380167d",
+    "url": "/react-recipe/static/js/main.2846479c.chunk.js"
+  },
+  {
+    "revision": "1f31941b1e9db2686091",
+    "url": "/react-recipe/static/js/runtime~main.dbd27b7b.js"
+  }
+]);
